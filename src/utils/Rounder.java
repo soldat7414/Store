886ced0dee82433a0159fbf0950db08cbece20d3
soldat7414 <soldat7414@gmail.com>
2022-04-05 12:19:20 +0300

    Add rounder

---
+++ src/utils/Rounder.java
@@ -0,0 +1,11 @@
+package utils;
+
+import java.text.DecimalFormat;
+
+public class Rounder {
+
+    public static String roundValue(double value) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        return df.format(value);
+    }
+}
